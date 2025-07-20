7677b02e6b9e3322c61a0b8d378959f817fe7c0e
Hari2821 <hariharan00619@gmail.com>
2025-07-20 20:38:39 +0000

    Update user change post installation steps for docker

--- docker_setup.sh
+++ docker_setup.sh
@@ -13,3 +13,9 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
